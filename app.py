ad56a80d16b92e833ca301df33e8ca1899af7215
Akshi Gupta <65853402+akshi-gupta123@users.noreply.github.com>
2023-06-01 19:14:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
